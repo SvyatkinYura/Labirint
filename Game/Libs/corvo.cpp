0c52c2492fd171e5c3a3ec2d71a1fe23ed269796
Mikhail Abramov <beavisabra@yandex.ru>
2017-10-03 19:22:05 +0300

    Правильная структура

---
+++ Game/Libs/corvo.cpp
@@ -0,0 +1,11 @@
+
+struct Corvo
+{
+    double x, y;
+    double respX, respY;
+    int hasSword; // 1 - с оружием, 0 - без
+    int hasBarrel; // 1 - с бочкой, 0 - без
+    int levelComplete; //1/0
+    int killguard;
+    HDC texture;
+};
