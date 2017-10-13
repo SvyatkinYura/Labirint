4daec89fbadaf8577c82946abe3ba57dbf9ce3b6
Mikhail Abramov <beavisabra@yandex.ru>
2017-10-13 17:50:31 +0300

    Считывание уровней из папки

---
+++ Libs/barrel.cpp
@@ -0,0 +1,28 @@
+#include "TXLib.h"
+#include "corvo.cpp"
+
+struct Barrel
+{
+    double x, y;
+    int taken;
+    HDC texture;
+};
+
+void drawBarrel(Barrel b)
+{
+    if (b.taken == 0)
+        txTransparentBlt(txDC(), b.x - 20, b.y - 20, 40, 68, b.texture, 0, 0, RGB(255, 255, 255));
+
+}
+
+void actBarrel (Barrel &b, Corvo &c)
+{
+    double dx = c.x - b.x;
+    double dy = c.y - b.y;
+    double distance = sqrt(dx * dx + dy * dy); //теорема Пифагора
+    if (distance < 20)
+    {
+        c.hasBarrel = 1;
+        b.taken = 1;
+    }
+}
