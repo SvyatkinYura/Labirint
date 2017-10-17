34495b3d27fa84d41ecf8481256671f36b375874
Voltronormu <olenenok1401@mail.ru>
2017-10-17 17:07:10 +0400

    vgfgyug7u

--- Game/main.cpp
+++ Game/main.cpp
@@ -26,6 +26,7 @@ int main()
     struct dirent *filename;
     char *dirname = "Levels\\";
     printf("%s\n", dirname);
+    int x = 0;
 
     if ((mydir = opendir (dirname)) != NULL)
     {
@@ -33,13 +34,32 @@ int main()
         {
             if ((strcmp(".", filename->d_name) !=0) and (strcmp("..", filename->d_name) != 0))
             {
-                printf("%s\n", filename->d_name);
-                createLevel(l0, filename->d_name);
+                txSetColor(TX_BLACK);
+                txRectangle(x, 0, x + 200, 100);
+                txTextOut(x, 0, filename->d_name);
+                x = x + 200;
+                //printf("%s\n", filename->d_name);
+                /*createLevel(l0, filename->d_name);
                 playLevel(l0);
-                destroyLevel(l0);
+                destroyLevel(l0);*/
             }
         }
         closedir (mydir);
+
+        while (!GetAsyncKeyState(VK_ESCAPE))
+        {
+            if (txMouseButtons() & 1
+            && txMouseX() >= 0
+            && txMouseX() <= 200
+            && txMouseY() >= 0
+            && txMouseY() <= 100)
+            {
+                createLevel(l0, "level0.txt");
+                playLevel(l0);
+                destroyLevel(l0);
+            }
+            txSleep(100);
+        }
     }
     else
     {
