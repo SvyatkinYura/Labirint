c4329604208e314d1df3c39f7d2b85d2c210b236
AnfimovS <31889055+AnfimovS@users.noreply.github.com>
2017-10-24 16:25:47 +0400

    Update main.cpp

--- Redactor/main.cpp
+++ Redactor/main.cpp
@@ -2,13 +2,18 @@
 #include "TXLib.h"
 using namespace std;
 
-void dButton(int x, int y, const char* text)
+struct Button {
+    int x,y;
+    const char* text;
+};
+
+void dButton(Button b)
 {
     txSetColor(RGB(0, 0, 0), 1);
     txSetFillColor(RGB(255, 255, 255));
     txSelectFont("Times New Roman", 25);
-    txRectangle(x, y, x + 150 , y + 50);
-    txTextOut(x + 10, y + 15, text);
+    txRectangle(b.x, b.y, b.x + 150 , b.y + 50);
+    txTextOut(b.x + 10, b.y + 15, b.text);
 }
 
 int main()
@@ -21,18 +26,25 @@ int main()
         txSetFillColor(TX_BLACK);
         txClear();
 
+        Button b[9];
+
+        b[0] = {0, 0, "Стена"};
+        b[1] = {0, 50, "Дорога"};
+        b[2] = {0, 100, "Главный герой"};
+        b[3] = {0, 150, "Вентиляция"};
+        b[4] = {0, 200, "Бочка"};
+        b[5] = {0, 250, "Ловушка"};
+        b[6] = {0, 300, "Кнопка"};
+        b[7] = {0, 350, "Охранник"};
+        b[8] = {0, 400, "Воидзона"};
+
         txSetColour(RGB(0, 0, 0), 1);
         txSetFillColour(RGB(255, 255, 255));
         txSelectFont("Times New Roman", 25);
-        dButton(0, 0, "Стена");
-        dButton(0, 50, "Дорога");
-        dButton(0, 100, "Главный герой");
-        dButton(0, 150, "Вентиляция");
-        dButton(0, 200, "Бочка");
-        dButton(0, 250, "Ловушка");
-        dButton(0, 300, "Кнопка");
-        dButton(0, 350, "Охранник");
-        dButton(0, 400, "Воидзона");
+        for (int Button_number = 0; Button_number < 9; Button_number++)
+        {
+            dButton(b[Button_number]);
+        }
 
         txSleep (10);
         txEnd();
