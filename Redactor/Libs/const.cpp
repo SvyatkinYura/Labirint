7efbf804521658291ac30b99045497ef687f4214
AnfimovS <31889055+AnfimovS@users.noreply.github.com>
2017-11-24 16:54:07 +0400

    Update const.cpp

--- Redactor/Libs/const.cpp
+++ Redactor/Libs/const.cpp
@@ -1,6 +1,6 @@
 #pragma once
 
 const int KOLICHESTVO_KNOPOK = 9;
-const int SHIRINA_KNOPKI = 150;
-const int VISOTA_KNOPKI = 50;
+const int SHIRINA_KNOPKI = 160;
+const int VISOTA_KNOPKI = 40;
 const int SHIRINA_OBJ = 20;
