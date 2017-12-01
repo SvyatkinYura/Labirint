28616c0bd62246005b1200cd3364aeb181fe9923
mabramovsd <beavisabra@yandex.ru>
2017-12-01 19:36:11 +0300

    Удаляем Энрико Ферми (aka папка реактора)

--- Game/Libs/corvo.cpp
+++ Game/Libs/corvo.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "const.cpp"
 
 struct Corvo
--- Redactor/Libs/const.cpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-const int KOLICHESTVO_KNOPOK = 9;
-const int SHIRINA_KNOPKI = 160;
-const int VISOTA_KNOPKI = 40;
-const int SHIRINA_OBJ = 20;
